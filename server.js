018fc82dbf511e8fa80819388f2dd164caa600a5
Martin <martiinotin24@outlook.com>
2026-03-10 13:58:57 -0700

    feat: initial express server setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); 
+
+app.get('/api/v1', (req, res) => {
+    res.json({ message: "¡Welcome to Gigmanager API!" });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
